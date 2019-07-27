08e0715bc2d775de5668dbbd8c56140d537031d1
betato <battlestargatticus@gmail.com>
2019-07-27 00:41:13 -0400

    Add GetViewRect to Camera

--- FactoryLand/Camera.cs
+++ FactoryLand/Camera.cs
@@ -26,5 +26,25 @@ namespace FactoryLand
         {
             return Matrix.CreateTranslation(new Vector3(-Location.X, -Location.Y, 0)) * Matrix.CreateScale(Zoom) * Matrix.CreateTranslation(new Vector3(Viewport.Width * 0.5f, Viewport.Height * 0.5f, 0));
         }
+
+        public Rectangle GetViewRect()
+        {
+            var inverseViewMatrix = Matrix.Invert(GetTransform());
+
+            Vector2 tl = Vector2.Transform(Vector2.Zero, inverseViewMatrix);
+            Vector2 tr = Vector2.Transform(new Vector2(Viewport.Bounds.X, 0), inverseViewMatrix);
+            Vector2 bl = Vector2.Transform(new Vector2(0, Viewport.Bounds.Y), inverseViewMatrix);
+            Vector2 br = Vector2.Transform(new Vector2(Viewport.Bounds.Width, Viewport.Bounds.Height), inverseViewMatrix);
+
+            Vector2 min = new Vector2(
+                MathHelper.Min(tl.X, MathHelper.Min(tr.X, MathHelper.Min(bl.X, br.X))),
+                MathHelper.Min(tl.Y, MathHelper.Min(tr.Y, MathHelper.Min(bl.Y, br.Y))));
+
+            Vector2 max = new Vector2(
+                MathHelper.Max(tl.X, MathHelper.Max(tr.X, MathHelper.Max(bl.X, br.X))),
+                MathHelper.Max(tl.Y, MathHelper.Max(tr.Y, MathHelper.Max(bl.Y, br.Y))));
+
+            return new Rectangle((int)min.X, (int)min.Y, (int)(max.X - min.X), (int)(max.Y - min.Y));
+        }
     }
 }
